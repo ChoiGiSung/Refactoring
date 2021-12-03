cbdc73f090f7921526dd4f36e9d4ac86faf7e9cd
gisungchoi <chlrltjd159@naver.com>
2021-12-03 17:48:18 +0900

    refactor : 임시변수를 메소드 호출로 전환
    - 결과를 저장하는데에만 사용됨
    - 임시변수는 최대한 삭제

--- src/main/java/chapter1/Customer.java
+++ src/main/java/chapter1/Customer.java
@@ -27,8 +27,6 @@ public class Customer {
 
         String result = this.name + "고객님의 대여 기록\n";
         for (Rental rental : rentals) {
-            double thisAmount = 0;
-            thisAmount = rental.getCharge();
 
             //적립 포인트를 1 포인트 증가
             frequentRenterPoints++;
@@ -38,10 +36,10 @@ public class Customer {
             }
 
             //이번에 대여하는 비디오 정보와 대여료를 출력
-            result += "\t" + rental.getMovie().getTitle() + "\n" + thisAmount + "\n";
+            result += "\t" + rental.getMovie().getTitle() + "\n" + rental.getCharge() + "\n";
 
             //현재까지 누적된 총 대여료
-            totalAmount += thisAmount;
+            totalAmount += rental.getCharge();
         }
 
         //푸터 행 추가
