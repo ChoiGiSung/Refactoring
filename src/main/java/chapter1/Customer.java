2811fb5bee3733a646a78a54cc5a72f40092796d
gisungchoi <chlrltjd159@naver.com>
2021-12-03 17:33:57 +0900

    refactor : 변수명 변경

--- src/main/java/chapter1/Customer.java
+++ src/main/java/chapter1/Customer.java
@@ -51,26 +51,26 @@ public class Customer {
     }
 
     private double amountFor(Rental rental) {
-        double thisAmount = 0;
+        double result = 0;
 
         //비디오 종류별 대여료 계산
         switch (rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
+                result += 2;
                 if (rental.getDaysRented() > 2) {
-                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                    result += (rental.getDaysRented() - 2) * 1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += rental.getDaysRented() * 3;
+                result += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDREN:
-                thisAmount += 1.5;
+                result += 1.5;
                 if (rental.getDaysRented() > 3) {
-                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                    result += (rental.getDaysRented() - 3) * 1.5;
                 }
                 break;
         }
-        return thisAmount;
+        return result;
     }
 }
