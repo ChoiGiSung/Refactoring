55160ef000f57b77fef0944554ed85cac7c5413f
gisungchoi <chlrltjd159@naver.com>
2021-12-03 17:53:27 +0900

    refactor : 메소드 추출
    - 책임을 rental에게

--- src/main/java/chapter1/Customer.java
+++ src/main/java/chapter1/Customer.java
@@ -29,11 +29,7 @@ public class Customer {
         for (Rental rental : rentals) {
 
             //적립 포인트를 1 포인트 증가
-            frequentRenterPoints++;
-            //최신물을 이틀 이상 대여하면 보너스 포인트 지급
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
+            frequentRenterPoints += rental.getFrequentRenterPoints();
 
             //이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + rental.getMovie().getTitle() + "\n" + rental.getCharge() + "\n";
--- src/main/java/chapter1/Rental.java
+++ src/main/java/chapter1/Rental.java
@@ -33,4 +33,13 @@ public class Rental {
         }
         return result;
     }
+
+    public int getFrequentRenterPoints() {
+        //기본은 1 포인트
+        //최신물을 이틀 이상 대여하면 보너스 포인트 지급
+        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
