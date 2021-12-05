da90bbebf7176339736688bc78d1238c9315f0f2
gisungchoi <chlrltjd159@naver.com>
2021-12-05 15:45:22 +0900

    refactor : 임시 변수 삭제, 메소드 분리

--- src/main/java/chapter6/Item.java
+++ src/main/java/chapter6/Item.java
@@ -9,13 +9,17 @@ public class Item {
     private int itemPrice;
 
     public double getPrice(){
+        return  getDiscountFactor() * getBasePrice();
+    }
+
+    private double getDiscountFactor() {
         double discountFactor;
         if(getBasePrice() > 1000){
             discountFactor = 0.95;
         }else {
             discountFactor = 0.98;
         }
-        return discountFactor * getBasePrice();
+        return discountFactor;
     }
 
     private int getBasePrice() {
