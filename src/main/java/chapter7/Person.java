269cf8a13bd8309b7f9725e582a4f43ce94b4d45
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2021-12-06 21:56:18 +0900

    feat : 예제 class 생성

---
+++ src/main/java/chapter7/Person.java
@@ -0,0 +1,14 @@
+package chapter7;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Person {
+
+    private String name;
+    private String tel;
+    private String telArea;
+
+}
